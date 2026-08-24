efdf933644a4678b96ad09dda70d2bd0fa228162
arbrim1030-art <arbrim1030@gmail.com>
2026-08-24 02:10:44 +0200

    test

---
+++ ft_printf.h
@@ -0,0 +1,14 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(char const *str, ...);
+int	ft_putchar(char c);
+int	ft_putnbr(long nb);
+int	ft_putnbr_hexa(unsigned long nbr, char up);
+int	ft_putptr(void *ptr);
+int	ft_putstr(char *str);
+
+#endif
